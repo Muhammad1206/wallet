922943e4de721cb10a76268396e75ab7913a63a7
studend-js <yakhshiboev@bk.ru>
2020-10-02 22:00:32 +0500

    Funcion for wallet

---
+++ pkg/wallet/service.go
@@ -0,0 +1,153 @@
+package wallet
+
+import (
+	"errors"
+
+	"github.com/Firdavs2002/wallet/pkg/types"
+	"github.com/google/uuid"
+)
+
+//ErrPhoneRegistered - телефон уже регитрирован
+var ErrPhoneRegistered = errors.New("phone already registred")
+
+//ErrAmountMustBePositive - счёт не может быть пустым
+var ErrAmountMustBePositive = errors.New("amount must be greater than zero")
+
+//ErrAccountNotFound - пользователь не найден
+var ErrAccountNotFound = errors.New("account not found")
+
+//ErrNotEnoughtBalance - на счете недостаточно средств
+var ErrNotEnoughtBalance = errors.New("account not enough balance")
+
+//ErrPaymentNotFound - платеж не найден
+var ErrPaymentNotFound = errors.New("payment not found")
+
+// Service представляет информацию о пользователе.
+type Service struct {
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
+}
+
+// RegisterAccount - метод для регистрация нового прользователя.
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+	s.nextAccountID++
+	account := &types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+	return account, nil
+}
+
+//Pay метод для регистрации платижа
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	if account.Balance < amount {
+		return nil, ErrNotEnoughtBalance
+	}
+
+	account.Balance -= amount
+
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID:        paymentID,
+		AccountID: accountID,
+		Amount:    amount,
+		Category:  category,
+		Status:    types.PaymentStatusInProgress,
+	}
+
+	s.payments = append(s.payments, payment)
+	return payment, nil
+}
+
+// FindAccountByID ищем пользователя по ID
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
+	var account *types.Account
+
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	return account, nil
+}
+
+// FindPaymentByID ищем платёж по ID
+func (s *Service) FindPaymentByID(paymentID string) (*types.Payment, error) {
+	var payment *types.Payment
+
+	for _, pay := range s.payments {
+		if pay.ID == paymentID {
+			payment = pay
+		}
+	}
+
+	if payment == nil {
+		return nil, ErrPaymentNotFound
+	}
+
+	return payment, nil
+}
+
+//Deposit method
+func (s *Service) Deposit(accountID int64, amount types.Money) error {
+	if amount < 0 {
+		return ErrAmountMustBePositive
+	}
+	account, err := s.FindAccountByID(accountID)
+	if err != nil {
+		return err
+	}
+	account.Balance += amount
+	return nil
+}
+
+// Reject метод для отмены покупок
+func (s *Service) Reject(paymentID string) error {
+	pay, err := s.FindPaymentByID(paymentID)
+
+	if err != nil {
+		return err
+	}
+
+	acc, err := s.FindAccountByID(pay.AccountID)
+
+	if err != nil {
+		return err
+	}
+
+	pay.Status = types.PaymentStatusFail
+	acc.Balance += pay.Amount
+
+	return nil
+}
